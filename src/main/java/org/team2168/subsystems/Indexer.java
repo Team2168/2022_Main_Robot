e321a7d90c1d423e216d258c5ce5d18fab5309bf
Shubbii <shubbiievii@gmail.com>
2022-01-15 13:31:21 -0500

    created Indexer class

---
+++ src/main/java/org/team2168/subsystems/Indexer.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team2168.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Indexer extends SubsystemBase {
+  /** Creates a new Indexer. */
+  public Indexer() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
