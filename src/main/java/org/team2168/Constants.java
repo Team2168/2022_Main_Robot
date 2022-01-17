5261641f570eab6b10dc07a39888b491a849a6c8
Katie Lam <katielam@team2168.org>
2022-01-17 17:37:25 -0500

    Organized and added stuff
    CanDigitalInput has been moved to the Utils folder, Gains has been added
    The turret motor device ID has been moved to Constants and gains were added for the turret motor.

--- src/main/java/org/team2168/Constants.java
+++ src/main/java/org/team2168/Constants.java
@@ -12,4 +12,7 @@ package org.team2168;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public final static int TALONFX_TURRET_MOTOR = 0;
+}
--- src/main/java/org/team2168/subsystems/Turret.java
+++ src/main/java/org/team2168/subsystems/Turret.java
@@ -5,26 +5,45 @@
 package org.team2168.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-import org.team2168.CanDigitalInput;
+import org.team2168.Constants;
+import org.team2168.utils.CanDigitalInput;
+import org.team2168.utils.Gains;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
   /** Creates a new Turret. */
   private CanDigitalInput hallEffectSensor;
-  //the name below was by Conor's request
-  private TalonFX turtle;
+  private TalonFX turretMotor;
   private static Turret instance = null;
   private final int TICKS_PER_REV = 2048;
 
-  public Turret() {
-    //0 is a placeholdeor, move to constants later
-    turtle = new TalonFX(0);
-    hallEffectSensor = new CanDigitalInput(turtle);
+  //gains
+  public static final int kPIDLoopIdx = 0;
+  public static final int kTimeoutMs = 30;
+  public static boolean kSensorPhase = true;
+  public static boolean kMotorInvert = false;
+  static final Gains kGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
 
-    turtle.configFactoryDefault();
+  public Turret() {
+    turretMotor = new TalonFX(Constants.TALONFX_TURRET_MOTOR);
+    hallEffectSensor = new CanDigitalInput(turretMotor);
+
+    turretMotor.configFactoryDefault();
+
+    turretMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, kPIDLoopIdx, kTimeoutMs);
+    turretMotor.setSensorPhase(kSensorPhase);
+    turretMotor.setInverted(kMotorInvert);
+
+    turretMotor.configAllowableClosedloopError(0, kPIDLoopIdx, kTimeoutMs);
+
+    turretMotor.config_kF(kPIDLoopIdx, kGains.kF, kTimeoutMs);
+    turretMotor.config_kP(kPIDLoopIdx, kGains.kP, kTimeoutMs);
+    turretMotor.config_kI(kPIDLoopIdx, kGains.kI, kTimeoutMs);
+    turretMotor.config_kD(kPIDLoopIdx, kGains.kD, kTimeoutMs);
   }
 
   public static Turret getInstance() {
@@ -42,11 +61,11 @@ public class Turret extends SubsystemBase {
    * @param rotation Should be between -1 and 1
    */
   public void rotateTurret(double rotation) {
-    turtle.set(ControlMode.Position, rotation * TICKS_PER_REV);
+    turretMotor.set(ControlMode.Position, rotation * TICKS_PER_REV);
   }
 
-  public double position() {
-    return turtle.getSelectedSensorPosition();
+  public double getposition() {
+    return turretMotor.getSelectedSensorPosition();
   }
 
   @Override
