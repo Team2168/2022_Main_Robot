8cb30a8d970805a7d412952e4d3caf91601b7918
sloshyyy <nathanjjank@gmail.com>
2022-01-28 21:14:47 -0500

    added my data

--- src/main/java/org/team2168/Constants.java
+++ src/main/java/org/team2168/Constants.java
@@ -35,9 +35,9 @@ public final class Constants {
     }
 
     public static final class Drivetrain {
-        public static final double ksVolts = 0.61421;
-        public static final double kvVoltSecondsPerMeter = 2.007;
-        public static final double kaVoltSecondsSquaredPerMeter = 0.60831;
+        public static final double ksVolts = 0.61411;
+        public static final double kvVoltSecondsPerMeter = 2.0492;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.42072;
         public static final double kPDriveVel = 2.8231; //2.8231;
 
         public static final double kTrackwidthMeters = 0.87;//fudged to approximately right. Actual = 0.65;
