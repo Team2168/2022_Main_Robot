5063a375538c45332da72960142fc101299e7bba
Shubbii <shubbiievii@gmail.com>
2022-02-02 19:21:10 -0500

    Added RetractLift command

---
+++ src/main/java/org/team2168/commands/climber/RetractLift.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team2168.commands.climber;
+
+import org.team2168.subsystems.Climber;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RetractLift extends CommandBase {
+  /** Creates a new RetractLift. */
+  Climber climber;
+  private double liftDescentVelocity = -3.0;
+
+  public RetractLift(Climber climber) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(climber);
+    this.climber = climber;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climber.setSpeed(liftDescentVelocity);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return climber.isAtZeroPosition();
+  }
+}
