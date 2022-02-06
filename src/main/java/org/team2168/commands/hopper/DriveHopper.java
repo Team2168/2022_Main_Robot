396cc1266259d338fff102c04da1071ef79fde4d
Thxndered <LilthunderYT@gmail.com>
2022-02-06 14:50:18 -0500

    Removed hardcoded 0.3 in DriveHopper and replaced it with speed.

--- src/main/java/org/team2168/commands/hopper/DriveHopper.java
+++ src/main/java/org/team2168/commands/hopper/DriveHopper.java
@@ -30,7 +30,7 @@ public class DriveHopper extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    hopper.driveHopper(0.3);
+    hopper.driveHopper(speed);
   }
 
   // Called once the command ends or is interrupted.
