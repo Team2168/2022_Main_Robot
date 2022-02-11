af4fa2aaf23fe043dbe789393f05c85ccab5a386
Jio Sookhai <jiovannizsook@grotonk12.org>
2022-02-11 15:36:44 -0800

    e

---
+++ src/main/java/org/team2168/commands/IntakeRoller/IntakeSpeed.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team2168.commands.IntakeRoller;
+
+import org.team2168.subsystems.IntakeRoller;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class IntakeSpeed extends CommandBase {
+ 
+  private IntakeRoller iRoller;
+  public IntakeSpeed() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    iRoller = new IntakeRoller(true);
+
+
+    addRequirements(iRoller);
+  }
+
+  // Called when the command is initially scheduled.
+  public void setterIntakeSpeed(){
+    iRoller.setRollerSpeed(0.5);
+  }
+  
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+   setterIntakeSpeed(); 
+   iRoller.isIntakeRollerOn();
+  }
+   
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    iRoller.resetIntakeMotor(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/org/team2168/commands/IntakeRoller/StopIntakeMotor.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team2168.commands.IntakeRoller;
+
+import org.team2168.subsystems.IntakeRoller;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class StopIntakeMotor extends CommandBase {
+  private IntakeRoller stopIntakeMotorOne;
+  private final double resetIntakeValue = 0;
+ 
+  public StopIntakeMotor() {
+stopIntakeMotorOne = new IntakeRoller(false);
+addRequirements(stopIntakeMotorOne);
+  }
+
+  // Called when the command is initially scheduled.
+ 
+  public void resetMotor(){
+   stopIntakeMotorOne.resetIntakeMotor(resetIntakeValue);
+ }
+
+  @Override 
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    resetMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
