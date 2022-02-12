345114637c20d4e3e6b910c71a5d4a370adbce4b
Shubbii <shubbiievii@gmail.com>
2022-02-11 20:20:02 -0500

    Enables limelight within initialize() method.

--- src/main/java/org/team2168/commands/drivetrain/DriveWithLimelight.java
+++ src/main/java/org/team2168/commands/drivetrain/DriveWithLimelight.java
@@ -26,7 +26,11 @@ public class DriveWithLimelight extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    if (!lime.isLimelightEnabled()) {
+      lime.enableLimelight();
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
