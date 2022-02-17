2732b9801e8c302fc2a3fb44f0a21408751e0efc
James <jcorcoran@thayermahan.com>
2022-02-17 17:22:08 -0500

    use conventional names formatting

--- src/main/java/org/team2168/subsystems/Pooper.java
+++ src/main/java/org/team2168/subsystems/Pooper.java
@@ -7,8 +7,6 @@ package org.team2168.subsystems;
 import org.team2168.Constants.PneumaticsDevices;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
@@ -16,10 +14,6 @@ import io.github.oblarg.oblog.annotations.Log;
 public class Pooper extends SubsystemBase implements Loggable {
 
   public static Pooper instance = null;
-
-  /**
-   * Pooper solenoid, it does the pooping
-   */
   private DoubleSolenoid pooperSolenoid;
 
   public static Pooper getInstance() {
@@ -35,29 +29,19 @@ public class Pooper extends SubsystemBase implements Loggable {
       PneumaticsDevices.POOPER_DOUBLE_SOLENOID_RETRACT);
   }
 
-  
   /**
-   * Extends the pooperSolenoid forward
+   * Extends the pooperSolenoid to the extended position 
    */
-  public void forward() {
+  public void extend() {
     pooperSolenoid.set(DoubleSolenoid.Value.kForward);
   }
 
 
   /**
-   * Retracts the pooperSolenoid backwards
+   * Retracts the pooperSolenoid to the retracted
    */
-  
-  public void backwards() {
+  public void retract() {
     pooperSolenoid.set(DoubleSolenoid.Value.kReverse);
-  }
-
-  /**
-   * Moves the pooperSolenoid to a normal position
-   */
-  
-  public void off() {
-    pooperSolenoid.set(DoubleSolenoid.Value.kOff);
   }
 
   /**
@@ -65,32 +49,18 @@ public class Pooper extends SubsystemBase implements Loggable {
    * @return true when pooper is extened
    */
   @Log(name = "extended?", rowIndex = 1, columnIndex = 1)
-  public boolean pooperExtended() {
+  public boolean isExtended() {
     return pooperSolenoid.get() == DoubleSolenoid.Value.kForward;
   }
 
-
-/**
- * 
- * @return true when pooper is retracted
- */
-  @Log(name = "retracted?", rowIndex = 1, columnIndex = 2)
-  public boolean pooperRetracted() {
-    return pooperSolenoid.get() == DoubleSolenoid.Value.kReverse;
-  }
-
-
   /**
    * 
-   * @return true when pooper is off
+   * @return true when pooper is retracted
    */
-  @Log(name = "off?", rowIndex = 1, columnIndex = 3)
-  public boolean pooperOff() {
-    return pooperSolenoid.get() == DoubleSolenoid.Value.kOff;
+  @Log(name = "retracted?", rowIndex = 1, columnIndex = 2)
+  public boolean isRetracted() {
+    return pooperSolenoid.get() == DoubleSolenoid.Value.kReverse;
   }
-
-  /** Creates a new Pooper. */
-  
 
   @Override
   public void periodic() {
