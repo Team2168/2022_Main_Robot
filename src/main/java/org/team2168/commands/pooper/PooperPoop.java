0e6b336e73ce5367442feb46a5542a9d47b641f7
James <jcorcoran@thayermahan.com>
2022-02-17 17:21:52 -0500

    let pneumatic commands end

--- src/main/java/org/team2168/commands/pooper/PooperPoop.java
+++ src/main/java/org/team2168/commands/pooper/PooperPoop.java
@@ -34,6 +34,6 @@ public class PooperPoop extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
