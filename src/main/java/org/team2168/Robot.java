f904d96b7938d5ca222369778d4e1ff67538e0ff
Shubbii <shubbiievii@gmail.com>
2022-02-26 17:23:09 -0500

    Made limelight enable for autonomous period

--- src/main/java/org/team2168/Robot.java
+++ src/main/java/org/team2168/Robot.java
@@ -5,6 +5,7 @@
 package org.team2168;
 
 import org.team2168.subsystems.Hood;
+import org.team2168.subsystems.Limelight;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -90,6 +91,7 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     Hood.getInstance().setMotorBrake();
     robotContainer.drivetrain.setMotorsBrake();
+    Limelight.getInstance().enableLimelight();
     autonomousCommand = robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
@@ -107,6 +109,7 @@ public class Robot extends TimedRobot {
   public void teleopInit() {
     Hood.getInstance().setMotorBrake();;
     robotContainer.drivetrain.setMotorsBrake();
+    Limelight.getInstance().pauseLimelight();
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
