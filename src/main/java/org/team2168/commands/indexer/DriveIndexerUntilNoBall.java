d353d04376319852709e2f805df692aebfdfbbaa
Katie Lam <katielam@team2168.org>
2022-02-26 15:16:08 -0500

    DriveIndexerUntilNoBall ends with setting the indexer speed to zero

--- src/main/java/org/team2168/commands/indexer/DriveIndexerUntilNoBall.java
+++ src/main/java/org/team2168/commands/indexer/DriveIndexerUntilNoBall.java
@@ -32,7 +32,9 @@ public class DriveIndexerUntilNoBall extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    indexer.drive(0.0);
+  }
 
   // Returns true when the command should end.
   @Override
