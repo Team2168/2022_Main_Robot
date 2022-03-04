a59461bac2e2aadbcc5a1fa10b6b7357419ee160
Katie Lam <katielam@team2168.org>
2022-03-04 13:11:11 -0500

    Updated shooter and hood for fenderHigh

--- src/main/java/org/team2168/subsystems/Hood.java
+++ src/main/java/org/team2168/subsystems/Hood.java
@@ -28,7 +28,7 @@ public class Hood extends SubsystemBase implements Loggable {
     AUTO_TARMAC_LINE(23.0),
     AUTO_LAUNCHPAD(26.7),
     FENDER_LOW(10.0),
-    FENDER_HIGH(5.0),
+    FENDER_HIGH(7.0),
     TARMAC_LINE(22.0),  // 20
     LAUNCHPAD(28.0),
     WALL_SHOT(37.0),
