8290b6dfa82f549fac794d867a7ab7df3fb62b67
Shubbii <shubbiievii@gmail.com>
2022-03-12 13:27:49 -0500

    Made robot wait until it's level for longer

--- src/main/java/org/team2168/commands/climber/WaitUntilLevelRobot.java
+++ src/main/java/org/team2168/commands/climber/WaitUntilLevelRobot.java
@@ -14,23 +14,41 @@ public class WaitUntilLevelRobot extends CommandBase {
   private double last_angle;
   private double safe_angle;
   private boolean safe_to_extend = false;
-  public WaitUntilLevelRobot(double angle) {
+  private int acceptableLoops;
+  private final static int DEFAULT_ACCEPTABLE_LOOPS = 50;
+  private int withinThresholdLoops = 0;
+
+  public WaitUntilLevelRobot(double angle, int acceptableLoops) {
     dt = Drivetrain.getInstance();
     safe_angle = angle;
     last_angle = safe_angle;
+    this.acceptableLoops = acceptableLoops;
+  }
+
+  public WaitUntilLevelRobot(double angle) {
+    this(angle, DEFAULT_ACCEPTABLE_LOOPS);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    withinThresholdLoops = 0;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     double current_angle = dt.getPitch() - dt.gyroOffset;
 
-    safe_to_extend = (current_angle <= safe_angle);
+    if (current_angle <= safe_angle) {
+      ++withinThresholdLoops;
+    }
+    else {
+      withinThresholdLoops = 0;
+    }
+
+    safe_to_extend = (current_angle <= safe_angle) && (withinThresholdLoops >= acceptableLoops);
 
     last_angle = current_angle;
   }
