b6253cfd50ffade1b83741d170be0935217d7a09
sloshyyy <nathanjjank@gmail.com>
2022-03-13 18:04:39 -0400

    [wip] started mocking up other autos

---
+++ src/main/java/org/team2168/commands/auto/LeftField4Ball.java
@@ -0,0 +1,45 @@
+package org.team2168.commands.auto;
+
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import org.team2168.commands.FireBalls;
+import org.team2168.commands.QueueBallsForShotNoStop;
+import org.team2168.commands.drivetrain.DriveWithLimelight;
+import org.team2168.commands.drivetrain.DriveXDistance;
+import org.team2168.commands.indexer.DriveIndexer;
+import org.team2168.commands.intakeraiseandlower.IntakeLower;
+import org.team2168.commands.shooter.WaitForShooterAtSpeed;
+import org.team2168.commands.shootingpositions.auto.AutoTarmacLine;
+import org.team2168.subsystems.*;
+
+public class LeftField4Ball extends SequentialCommandGroup {
+    public LeftField4Ball(Drivetrain drivetrain,
+                             IntakeRaiseAndLower intakeRaiseAndLower,
+                             IntakeRoller intakeRoller,
+                             Hopper hopper,
+                             Indexer indexer,
+                             Hood hood,
+                             Shooter shooter,
+                             Pooper pooper,
+                             ColorSensor colorSensor,
+                             Limelight lime) {
+        addCommands(
+                new AutoTarmacLine(hood, shooter, lime).withTimeout(0.2),
+                new IntakeLower(intakeRaiseAndLower),
+                race(
+                        new QueueBallsForShotNoStop(hopper, indexer, pooper, colorSensor, intakeRoller),
+                        new DriveXDistance(drivetrain, -3.28084 * 12)
+                ),
+                new DriveIndexer(indexer, () -> 0.0).withTimeout(0.1),
+
+                new InstantCommand(() -> System.out.println("reving up shooter!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
+                new WaitForShooterAtSpeed(shooter),
+                new InstantCommand(() -> System.out.println("shooter done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11")),
+                new DriveWithLimelight(drivetrain, lime, 1.5, true),
+                new InstantCommand(() -> System.out.println("Limelight done!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
+                new FireBalls(shooter, indexer, hopper),
+                new WaitForShooterAtSpeed(shooter),
+                new FireBalls(shooter, indexer, hopper)
+        );
+}
