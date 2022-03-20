05d30085038984839a47020f9ce286d1a625128c
sloshyyy <nathanjjank@gmail.com>
2022-03-20 14:04:12 -0400

    Revert "stop turret for second part of 3 ball"
    This reverts commit bae11d759fb3b13b4bdb80029641ff7a789928d0.

--- src/main/java/org/team2168/commands/auto/pathplanner/ThreeBall.java
+++ src/main/java/org/team2168/commands/auto/pathplanner/ThreeBall.java
@@ -19,7 +19,6 @@ import org.team2168.commands.shootingpositions.TarmacLine;
 import org.team2168.commands.shootingpositions.auto.AutoTarmacLine;
 import org.team2168.commands.turret.DriveTurretWithLimelight;
 import org.team2168.commands.turret.RotateTurret;
-import org.team2168.commands.turret.StopTurret;
 import org.team2168.subsystems.*;
 import org.team2168.utils.PathUtil;
 
@@ -40,64 +39,56 @@ public class ThreeBall extends SequentialCommandGroup {
         Paths paths = Paths.getInstance();
         addCommands(
                 new RotateTurret(turret, 0.0).withTimeout(0.2),
-                race(
+                parallel(
                         new DriveTurretWithLimelight(turret, lime),
                         sequence(
-                                new AutoTarmacLine(hood, shooter, lime).withTimeout(0.2),
-                                new IntakeLower(intakeRaiseAndLower),
-                                race(  // run group until path ends
-                                        new QueueBallsForShotNoStop(hopper, indexer, pooper, colorSensor, intakeRoller),
-                                        PathUtil.getPathCommand(paths.path_4BALL_0, drivetrain, PathUtil.InitialPathState.DISCARDHEADING)
-                                ),
-                                // new RetractAndStopIntake(intakeRaiseAndLower, intakeRoller).withTimeout(0.1),
-                                new DriveIndexer(indexer, () -> 0.0).withTimeout(0.1),
+                            new AutoTarmacLine(hood, shooter, lime).withTimeout(0.2),
+                            new IntakeLower(intakeRaiseAndLower),
+                            race (  // run group until path ends
+                                    new QueueBallsForShotNoStop(hopper, indexer, pooper, colorSensor, intakeRoller),
+                                    PathUtil.getPathCommand(paths.path_4BALL_0, drivetrain, PathUtil.InitialPathState.DISCARDHEADING)
+                            ),
+                            // new RetractAndStopIntake(intakeRaiseAndLower, intakeRoller).withTimeout(0.1),
+                            new DriveIndexer(indexer, () -> 0.0).withTimeout(0.1),
 
-                                new InstantCommand(() -> System.out.println("reving up shooter!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
-                                new WaitForShooterAtSpeed(shooter),
-                                new InstantCommand(() -> System.out.println("shooter done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11")),
-                                new WaitForLimelightInPosition(lime),
-                                new InstantCommand(() -> System.out.println("Limelight done!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
-                                new FireBalls(shooter, indexer, hopper),
-                                new WaitForShooterAtSpeed(shooter),
-                                new FireBalls(shooter, indexer, hopper)
-                        )),
-                new RotateTurret(turret, 0.0).withTimeout(0.2), // zero turret after first set
-                new StopTurret(turret),
+                            new InstantCommand(() -> System.out.println("reving up shooter!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
+                            new WaitForShooterAtSpeed(shooter),
+                            new InstantCommand(() -> System.out.println("shooter done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11")),
+                            new WaitForLimelightInPosition(lime),
+                            new InstantCommand(() -> System.out.println("Limelight done!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
+                            new FireBalls(shooter, indexer, hopper),
+                            new WaitForShooterAtSpeed(shooter),
+                            new FireBalls(shooter, indexer, hopper),
 
-                sequence(
-                        new IntakeLower(intakeRaiseAndLower),
-                        // new Launchpad(hood, shooter, lime),
-                        new AutoTarmacLine(hood, shooter, lime).withTimeout(0.2),
-                        race(
-                                new QueueBallsForShotNoStop(hopper, indexer, pooper, colorSensor, intakeRoller),
-                                sequence(
-                                        PathUtil.getPathCommand(paths.path_3Ball_1, drivetrain, PathUtil.InitialPathState.PRESERVEODOMETRY)
-                                        // PathUtil.getPathCommand(paths.path_3Ball_2, drivetrain, PathUtil.InitialPathState.PRESERVEODOMETRY),
-                                        //new DriveWithLimelight(drivetrain, lime, 1.5, false)
-                                )
-                        ),
 
-                        // new RetractAndStopIntake(intakeRaiseAndLower, intakeRoller).withTimeout(0.1),
-                        parallel(
-                                new DriveTurretWithLimelight(turret, lime),
-                                sequence(
+                            new IntakeLower(intakeRaiseAndLower),
+                            // new Launchpad(hood, shooter, lime),
+                            new AutoTarmacLine(hood, shooter, lime).withTimeout(0.2),
+                            race (
+                                    new QueueBallsForShotNoStop(hopper, indexer, pooper, colorSensor, intakeRoller),
+                                    sequence(
+                                            PathUtil.getPathCommand(paths.path_3Ball_1, drivetrain, PathUtil.InitialPathState.PRESERVEODOMETRY)
+                                            // PathUtil.getPathCommand(paths.path_3Ball_2, drivetrain, PathUtil.InitialPathState.PRESERVEODOMETRY),
+                                            //new DriveWithLimelight(drivetrain, lime, 1.5, false)
+                                    )
+                            ),
 
-                                        new InstantCommand(() -> System.out.println("path finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
-                                        race(
-                                                new SetIntakeSpeed(intakeRoller, Constants.MotorSpeeds.INTAKE_SPEED),
-                                                sequence(
-                                                        new WaitForLimelightInPosition(lime),
-                                                        new FireBalls(shooter, indexer, hopper)
-                                                )
-                                        ),
+                            // new RetractAndStopIntake(intakeRaiseAndLower, intakeRoller).withTimeout(0.1),
+                            new InstantCommand(() -> System.out.println("path finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
+                            race (
+                                    new SetIntakeSpeed(intakeRoller, Constants.MotorSpeeds.INTAKE_SPEED),
+                                    sequence (
+                                            new WaitForLimelightInPosition(lime),
+                                            new FireBalls(shooter, indexer, hopper)
+                                    )
+                            ),
 
-                                        new InstantCommand(() -> System.out.println("fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
-                                        race(
-                                                new SetIntakeSpeed(intakeRoller, Constants.MotorSpeeds.INTAKE_SPEED),
-                                                new FireBalls(shooter, indexer, hopper)
-                                        )
-                                )
-                        )
+                            new InstantCommand(() -> System.out.println("fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")),
+                            race (
+                                    new SetIntakeSpeed(intakeRoller, Constants.MotorSpeeds.INTAKE_SPEED),
+                                    new FireBalls(shooter, indexer, hopper)
+                    )
+                )
                 )
         );
     }
